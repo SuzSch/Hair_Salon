540c23d7e3da3c4d6af7453765924216a0c8ca73
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-28 10:04:52 -0700

    update models and controllers, begin view for client index.

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace HairSalon.Models
+{
+public class Client
+{
+  public string ClientId { get; set;}
+  public string Name { get; set;}
+  public int PhoneNumber  { get; set;}
+}
+
+
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,10 @@
+namespace HairSalon.Models;
+
+public class Stylist
+{
+    public int StylistId { get; set; }
+
+    public string Name { get; set; }
+    public string Speciality { get; set; }
+    public string DayOff { get; set; }
+}
