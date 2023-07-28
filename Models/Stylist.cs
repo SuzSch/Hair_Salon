5a92d4360f4ca9187db34f279835fc6f3d3fe7e8
Suzanne Schuber <suzanneschuber@gmail.com>
2023-07-28 09:25:29 -0700

    set up file for project, add .gitignore.

---
+++ Models/Stylist.cs
@@ -0,0 +1,8 @@
+namespace HairSalon.Solutions.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
